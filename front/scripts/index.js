34ab4f75cf4cf459f67b1a9a99aae197321a9330
LucilaTineo <lucilamtineo@gmail.com>
2024-03-05 10:00:02 -0300

    nuevos cambios

--- front/scripts/index.js
+++ front/scripts/index.js
@@ -1,1 +1,15 @@
+tempData.map((item)=>{
+    const html = `
+        <div class = "pelicula-card">
+            <img src="${item.poster}" class ="pelicula-imagen" alt= "${item.title}"></img>
+                <div class= "pelicula-info">
+                    <h2 class= "pelicula-titulo">${item.title}</h2>
+                    <p class= "pelicula-genero">${item.genre.join(", ")}</p>
+                    <p class = "pelicula-director">Director: ${item.director}</p>
+                </div>
+        </div>
+    `;
+document.getElementById("contenedor-peliculas").innerHTML+=html
+});
 console.log(tempData);
+
